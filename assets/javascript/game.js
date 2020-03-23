2c90ce7e8f851c342776585187b3d674a7d6de38
Melanie Jindali <melaniejindali.dm@gmail.com>
2020-03-23 18:03:12 -0500

    Add functionality to crytals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,47 @@
-
 $(document).ready(function() {
 
-    var targetNumber = 0;
-    var randomNum = 0;
+    // targetNumber is randomly generated between 19 and 120
+    function randomNum() {
+    var targetNumber = Math.floor(Math.random() * 102) + 19;
+    $("#target-num").html(targetNumber);
+    }
+
+    // totalScore starting at 0
+    var totalScore = 0;
+    $("#total-score").text(totalScore);
+
     var wins = 0;
     var losses = 0;
-    var totalScore = 0;
-
+  
     // Crystals will generate numbers between 1 and 12
     var amethyst = Math.floor(Math.random() * 12) + 1;
     var sapphire = Math.floor(Math.random() * 12) + 1;
     var opal = Math.floor(Math.random() * 12) + 1;
     var peridot = Math.floor(Math.random() * 12) + 1;
 
+    // Set functionality to crystals
+    $("#amethyst").on('click', function() {
+            totalScore = totalScore + amethyst
+            $('#total-score').html(totalScore);
+    });        
+
+    $("#sapphire").on('click', function() {
+        totalScore = totalScore + sapphire
+            $('#total-score').html(totalScore);
+    });        
+
+    $("#opal").on('click', function() {
+        totalScore = totalScore + opal
+            $('#total-score').html(totalScore);
+    });      
+            
+    $("#peridot").on('click', function() {
+        totalScore = totalScore + peridot
+            $('#total-score').html(totalScore);                     
+    });       
+
+});
 
 
 
-var winsText = document.getElementById("wins")
-var lossesText = document.getElementById("losses")
-var totalScoreText = document.getElementById("total-score")
-var randomNumberText = document.getElementById("random-num")
 
